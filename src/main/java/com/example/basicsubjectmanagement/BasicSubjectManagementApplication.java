9aa7ffce9535179fcd7fc5933ccbd92dbe7c4f1e
Lucky-Star-999 <theboost1305@gmail.com>
2023-12-23 20:12:44 +0700

    Init

---
+++ src/main/java/com/example/basicsubjectmanagement/BasicSubjectManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicsubjectmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSubjectManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSubjectManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/basicsubjectmanagement/BasicSubjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basicsubjectmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSubjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
